f503e86fb9327c8c1b7c94d6d3932678d2b60c18
MaksimsTerjohins <55734404+MaksimsTerjohins@users.noreply.github.com>
2020-01-05 15:54:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
         $.ajax({
           'type' : 'GET',
           'dataType' : 'json',
-          'url' : 'https://combinatronics.com/MaksimsTerjohins/jscript/master/main.js',		  
-
+          'url' : 'http://192.168.1.102:8000/distance_R.txt',	  
           'statusCode' : {
             404: function(){
                 let msg = 'File not found';
